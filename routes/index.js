67592cc284379b72f92ce10a96026de7dfba175e
BtoBastian <bastianoppermann1997@gmail.com>
2017-07-22 15:52:42 +0200

    Converted to a fresh express installation

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(request, response, next) {
-
-    var customColor1 = request.cookies["custom-color1"];
-    customColor1 = customColor1 === undefined ? 'teal' : customColor1;
-
-    response.render('static/index', {
-        user: request.user === undefined ? null : request.user,
-        loggedIn: request.user != undefined,
-        customColor1: customColor1
-    });
-
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- util/config.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-// Very very simple config. No validation, just reading the file.
-module.exports = JSON.parse(fs.readFileSync(__dirname + '/../config.json'));
