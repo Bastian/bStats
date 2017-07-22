e26999cf1f55dba3e2a576da4738dee04c6016af
BtoBastian <bastianoppermann1997@gmail.com>
2017-07-22 17:19:31 +0200

    Express now uses the cluster module

---
+++ bin/start.js
@@ -0,0 +1,18 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    const www = require('./www');
+    console.log(`Worker ${process.pid} started`);
+}
