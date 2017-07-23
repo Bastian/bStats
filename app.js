4563aa7aabbd05f6bee851c8b7e5ed80b97a141e
BtoBastian <bastianoppermann1997@gmail.com>
2017-07-23 14:22:04 +0200

    Removed auto generated routes and views

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
-
 const app = express();
 
 // view engine setup
@@ -21,9 +18,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
