52ea36a5cd4ac8c75b57496b4a2e46eb04d3fd43
BtoBastian <bastianoppermann1997@gmail.com>
2017-07-23 14:56:48 +0200

    Uncommented app.use(...) for favicon

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
