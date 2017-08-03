44c54b201087a7e07bb621b2fae5db7b4c77793c
BtoBastian <bastianoppermann1997@gmail.com>
2017-08-03 16:57:08 +0200

    Removed MySQL from databaseManager and config as it's no longer needed

--- util/databaseManager.js
+++ util/databaseManager.js
@@ -1,30 +1,7 @@
-const mysql = require('mysql');
 const config = require('./config');
 const Redis = require('ioredis');
 
-let pools = {};
 let cluster = null;
-
-/**
- * Gets a connection pool by its name or create one if it does not exist.
- *
- * @param {string} poolName The name of the connection pool.
- * @returns {object} A connection pool with the given name.
- */
-function getMySQLConnectionPool(poolName) {
-    if (pools[poolName] !== undefined) {
-        return pools[poolName];
-    }
-    pools[poolName] = mysql.createPool({
-        connectionLimit : 10,
-        host     : config.mysql.host,
-        user     : config.mysql.user,
-        password : config.mysql.password,
-        database : config.mysql.database,
-        timeout: 300000 // 5 minutes
-    });
-    return pools[poolName];
-}
 
 /**
  * Gets the redis cluster defined in the config.
