a66f2cdd908cb15942b1a4ba9e3d23701a847119
BtoBastian <bastianoppermann1997@gmail.com>
2017-08-07 16:06:13 +0200

    Improved dataManager

--- util/dataManager.js
+++ util/dataManager.js
@@ -2,15 +2,6 @@ const databaseManager = require('./databaseManager');
 const timeUtil = require('../util/timeUtil');
 
 /**
- * Gets an unordered array with all plugin ids.
- */
-function getPluginIds() {
-    databaseManager.smembers('plugins.ids', function (err, res) {
-        return res;
-    });
-}
-
-/**
  * Gets a software by its id.
  * The method accepts two or three parameters in the following order:
  * - "id", "callback"
@@ -19,7 +10,7 @@ function getPluginIds() {
 function getSoftwareById() {
     let id = arguments[0];
     let fields = arguments.length === 3 ? arguments[1] :
-        ['name', 'url', 'globalPlugin', 'metricsClass', 'classCreation', 'maxRequestsPerIp', 'defaultCharts'];
+        ['name', 'url', 'globalPlugin', 'metricsClass', 'examplePlugin', 'maxRequestsPerIp', 'defaultCharts'];
     let callback = arguments.length === 3 ? arguments[2] : arguments[1];
     databaseManager.getRedisCluster().hmget(`software:${id}`, fields, function (err, res) {
         if (err || res === null) {
@@ -53,7 +44,7 @@ function getSoftwareById() {
 function getSoftwareByUrl() {
     let url = arguments[0].toLowerCase();
     let fields = arguments.length === 3 ? arguments[1] :
-        ['name', 'url', 'globalPlugin', 'metricsClass', 'classCreation', 'maxRequestsPerIp', 'defaultCharts'];
+        ['name', 'url', 'globalPlugin', 'metricsClass', 'examplePlugin', 'maxRequestsPerIp', 'defaultCharts'];
     let callback = arguments.length === 3 ? arguments[2] : arguments[1];
     databaseManager.getRedisCluster().get(`software.index.id.url:${url}`, function (err, res) {
         if (err) {
@@ -75,16 +66,16 @@ function getSoftwareByUrl() {
  * - "id", "fields", "callback"
  */
 function getPluginById() {
-    let id = arguments[0];
+    let id = parseInt(arguments[0]);
     let fields = arguments.length === 3 ? arguments[1] :
         ['name', 'software', 'charts', 'owner', 'global'];
     let callback = arguments.length === 3 ? arguments[2] : arguments[1];
     databaseManager.getRedisCluster().hmget(`plugins:${id}`, fields, function (err, res) {
         if (err || res === null) {
-            callback(err, res);
+            callback(err, null);
             return;
         }
-        let result = { id: parseInt(id) };
+        let result = { id: id };
         for (let i = 0; i < fields.length; i++) {
             switch (fields[i]) {
                 case 'charts':
@@ -158,7 +149,7 @@ function getGlobalPluginBySoftwareUrl() {
  * - "uid", "fields", "callback"
  */
 function getChartByUid() {
-    let uid = arguments[0];
+    let uid = parseInt(arguments[0]);
     let fields = arguments.length === 3 ? arguments[1] :
         ['id', 'type', 'position', 'title', 'default', 'data'];
     let callback = arguments.length === 3 ? arguments[2] : arguments[1];
@@ -195,8 +186,8 @@ function getChartByUid() {
  * - "plugin id", "chart id", "fields", "callback"
  */
 function getChartByPluginIdAndChartId() {
-    let pluginId = arguments[0].toLowerCase();
-    let chartId = arguments[1].toLowerCase();
+    let pluginId = arguments[0];
+    let chartId = arguments[1];
     let fields = arguments.length === 4 ? arguments[2] :
         ['id', 'type', 'position', 'title', 'default', 'data'];
     let callback = arguments.length === 4 ? arguments[3] : arguments[2];
@@ -211,6 +202,72 @@ function getChartByPluginIdAndChartId() {
             callback(err, null);
         }
     });
+}
+
+/**
+ * Gets an unordered array with all plugin ids.
+ */
+function getAllPluginIds(callback) {
+    databaseManager.getRedisCluster().smembers('plugins.ids', function (err, res) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        let data = [];
+        for (let i = 0; i < res.length; i++) {
+            data.push(parseInt(res[i]));
+        }
+        callback(null, res);
+    });
+}
+
+/**
+ * Gets the data of a line chart. The data is limited to a specific amount.
+ */
+function getLimitedLineChartData(chartUid, line, amount, callback) {
+    let startDate = timeUtil.tms2000ToDate(timeUtil.dateToTms2000(new Date()) - 1).getTime() - 1000*60*30*amount;
+    let datesToFetch = [];
+    for (let i = 0; i < amount; i++) {
+        startDate += 1000*60*30;
+        datesToFetch.push(startDate);
+    }
+
+    databaseManager.getRedisCluster().hmget(`data:${chartUid}.${line}`, datesToFetch, function (err, res) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        let data = [];
+        for (let i = 0; i < res.length; i++) {
+            if (!isNaN(parseInt(res[i]))) {
+                data.push([datesToFetch[i], parseInt(res[i])]);
+            } else {
+                data.push([datesToFetch[i], 0]);
+            }
+        }
+        callback(null, data);
+    });
+}
+
+/**
+ * Gets all stored data of a line chart.
+ */
+function getFullLineChartData(chartUid, line, callback) {
+    databaseManager.getRedisCluster().hgetall(`data:${chartUid}.${line}`, function (err, res) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        let data = [];
+        for (let i = 0; i < res.length; i += 2) {
+            data.push([parseInt(res[i*2]), parseInt(res[i*2+1])]);
+        }
+        callback(null, data);
+    });
+}
+
+function getPieData(chartUid, callback) {
+
 }
 
 /**
@@ -238,7 +295,7 @@ function updateMapData(chartUid, tms2000, valueName, value) {
  * Updates the data for the chart with the given uid. The chart must be a line chart.
  */
 function updateLineChartData(chartUid, value, line, tms2000) {
-    databaseManager.getRedisCluster().hincrby(`data:${chartUid}.${line}`, timeUtil.tms2000ToDate(tms2000).getUTCDate(), value, function (err, res) {
+    databaseManager.getRedisCluster().hincrby(`data:${chartUid}.${line}`, timeUtil.tms2000ToDate(tms2000).getTime(), value, function (err, res) {
         if (err) {
             console.log(err);
         }
@@ -259,8 +316,7 @@ function updateBarData(chartUid, tms2000, category, values) {
     // TODO
 }
 
-// Methods to access existing data
-module.exports.getPluginIds = getPluginIds;
+// Methods to access existing structure data
 module.exports.getSoftwareByUrl = getSoftwareByUrl;
 module.exports.getSoftwareById = getSoftwareById;
 module.exports.getPluginById = getPluginById;
@@ -268,6 +324,11 @@ module.exports.getPluginBySoftwareUrlAndName = getPluginBySoftwareUrlAndName;
 module.exports.getGlobalPluginBySoftwareUrl = getGlobalPluginBySoftwareUrl;
 module.exports.getChartByUid = getChartByUid;
 module.exports.getChartByPluginIdAndChartId = getChartByPluginIdAndChartId;
+module.exports.getAllPluginIds = getAllPluginIds;
+
+// Methods to access existing chart data
+module.exports.getLimitedLineChartData = getLimitedLineChartData;
+module.exports.getFullLineChartData = getFullLineChartData;
 
 // Methods for updating existing data
 module.exports.updatePieData = updatePieData;
