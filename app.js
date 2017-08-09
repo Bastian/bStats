2ce70d24104c85803e85776bb6477adc033b5883
BtoBastian <bastianoppermann1997@gmail.com>
2017-08-09 12:21:49 +0200

    Added missing app.use(...) for logout route

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.use(function (req, res, next) {
 
 app.use('/', require('./routes/index'));
 app.use('/login', require('./routes/login'));
+app.use('/logout', require('./routes/logout'));
 app.use('/register', require('./routes/register'));
 app.use('/submitData', require('./routes/submitData'));
 app.use('/api/v1/plugins', require('./routes/api/v1/plugin'));
