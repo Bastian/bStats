eaa3ad7e688ea52124c525f05bcecd429610f672
BtoBastian <bastianoppermann1997@gmail.com>
2017-10-04 20:36:52 +0200

    Added credits page

--- app.js
+++ app.js
@@ -102,6 +102,7 @@ app.use('/getting-started', require('./routes/gettingStarted'));
 app.use('/help/custom-charts', require('./routes/customCharts'));
 app.use('/imprint', require('./routes/imprint'));
 app.use('/privacy-policy', require('./routes/privacyPolicy'));
+app.use('/credits', require('./routes/credits'));
 
 // Api v1
 app.use('/api/v1/plugins', require('./routes/api/v1/plugin'));
