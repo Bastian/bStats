d0d3a36592618f6fe536873c02297d0b796f84bb
Bastian Oppermann <bastianoppermann1997@gmail.com>
2020-01-20 15:06:07 +0100

    Detect more java versions (Java 11-20)

--- routes/submitData.js
+++ routes/submitData.js
@@ -180,6 +180,36 @@ router.post('/:software?', function(request, response, next) {
                             } else if (javaVersion.startsWith("10")) {
                                 javaVersionChart.data.values['Java 10'] = {};
                                 javaVersionChart.data.values['Java 10'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("11")) {
+                                javaVersionChart.data.values['Java 11'] = {};
+                                javaVersionChart.data.values['Java 11'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("12")) {
+                                javaVersionChart.data.values['Java 12'] = {};
+                                javaVersionChart.data.values['Java 12'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("13")) {
+                                javaVersionChart.data.values['Java 13'] = {};
+                                javaVersionChart.data.values['Java 13'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("14")) {
+                                javaVersionChart.data.values['Java 14'] = {};
+                                javaVersionChart.data.values['Java 14'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("15")) {
+                                javaVersionChart.data.values['Java 15'] = {};
+                                javaVersionChart.data.values['Java 15'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("16")) {
+                                javaVersionChart.data.values['Java 16'] = {};
+                                javaVersionChart.data.values['Java 16'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("17")) {
+                                javaVersionChart.data.values['Java 17'] = {};
+                                javaVersionChart.data.values['Java 17'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("18")) {
+                                javaVersionChart.data.values['Java 18'] = {};
+                                javaVersionChart.data.values['Java 18'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("19")) {
+                                javaVersionChart.data.values['Java 19'] = {};
+                                javaVersionChart.data.values['Java 19'][javaVersion] = 1;
+                            } else if (javaVersion.startsWith("20")) {
+                                javaVersionChart.data.values['Java 20'] = {};
+                                javaVersionChart.data.values['Java 20'][javaVersion] = 1;
                             } else {
                                 javaVersionChart.data.values['Other'] = {};
                                 javaVersionChart.data.values['Other'][javaVersion] = 1;
